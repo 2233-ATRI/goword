bb7df1e2099afc2df9167d2717ffec4bfee43c96
2233-ATRI <93872027+2233-ATRI@users.noreply.github.com>
2023-02-08 19:41:56 +0800

    1

--- c12/mo21/main/main2.go
+++ c12/mo21/main/main2.go
@@ -1,5 +1,6 @@
 package main
 
+//数组模拟环形队列
 import (
 	"errors"
 	"fmt"
@@ -21,10 +22,32 @@ func (this *circleQueue) push(val int) (err error) {
 	if this.isfull() {
 		return errors.New("queue full")
 	}
-
+	//最后一个数据位不可以存元素，不然无法进行环形化
+	this.array[this.tail] = val
+	this.tail = (this.tail + 1) % this.maxsize
+	return
 }
 
-func (this *circleQueue) pop(val int) (val int, err error) {
+func (this *circleQueue) pop() (val int, err error) {
+	if this.isempty() {
+		return 0, errors.New("queue empty")
+	}
+	//取出hand是指向队首，包含队首元素
+	val = this.array[this.hand]
+	this.hand = (this.hand + 1) % this.maxsize
+	return
+}
+
+func (this *circleQueue) listqueue() {
+	size := this.size()
+	if size == 0 {
+		fmt.Println("队列为空")
+	}
+	temphead := this.hand
+	for i := 0; i < size; i++ {
+		fmt.Printf("arr[%d]=%d\t", temphead, this.array[temphead])
+		temphead = (temphead + 1) % this.maxsize
+	}
 
 }
 
@@ -43,30 +66,41 @@ func (this *circleQueue) size() int {
 }
 
 func main() {
+	queue := &circleQueue{
+		maxsize: 5,
+		hand:    0,
+		tail:    0,
+	}
 	var key string
 	var val int
 
 	for {
-		fmt.Printf("1,输入add，表示获取数据到队列")
-		fmt.Printf("2,输入get，表示从队列获取数据")
-		fmt.Printf("3,输入show，表示显示队列")
-		fmt.Printf("4,输入exit，表示退出")
+		fmt.Printf("1,输入add，表示获取数据到队列\n")
+		fmt.Printf("2,输入get，表示从队列获取数据\n")
+		fmt.Printf("3,输入show，表示显示队列\n")
+		fmt.Printf("4,输入exit，表示退出\n")
 
 		fmt.Scanln(&key)
 		switch key {
 		case "add":
 			fmt.Println("输入要入队列数")
 			fmt.Scanln(&val)
-			//err := queue.addqueue(val)
-			//if err != nil {
-			//	fmt.Printf("存在错误", err.Error())
-			//} else {
-			//	fmt.Printf("加入成功")
-			//}
+			err := queue.push(val)
+			if err != nil {
+				fmt.Printf("存在错误", err.Error())
+			} else {
+				fmt.Printf("加入成功")
+			}
 		case "get":
 			fmt.Println("get")
+			val, err := queue.pop()
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				fmt.Println("取出了一个数为 =", val)
+			}
 		case "show":
-			//queue.showqueue()
+			queue.listqueue()
 		case "exit":
 			os.Exit(0)
 		}
---
+++ c12/mo21/main/main3.go
@@ -0,0 +1,120 @@
+package main
+
+import "fmt"
+
+type heronode struct {
+	no       int
+	name     string
+	nickname string
+	next     *heronode
+}
+
+func inserhero(x *heronode, newheronode *heronode) {
+	heee := x
+	for {
+		if heee == nil {
+			break
+		}
+		x = x.next
+	}
+	x.next = newheronode
+}
+
+func Modifyhero(head *heronode, id int) {
+	heee := head
+	flag := false
+	for {
+		if heee.next == nil {
+			break
+		} else if heee.next.no == id {
+			flag = true
+			break
+		}
+		heee = heee.next
+	}
+	var a string
+	var b string
+	if flag {
+		fmt.Scanln(&a)
+		heee.name = a
+		fmt.Scanln(&b)
+		heee.nickname = b
+	}
+}
+
+// 插入可以根据no大小进行适当的插入
+func inserhero2(x *heronode, newheronode *heronode) {
+	heee := x
+	flag := true
+	for {
+		if heee.next == nil {
+			break
+		} else if heee.next.no > newheronode.no {
+			break
+		} else if heee.next.no == newheronode.no {
+			fmt.Println("错误")
+			flag = false
+			break
+		}
+		x = x.next
+	}
+
+	if !flag {
+		fmt.Println("已经存在", newheronode.no)
+		return
+	} else {
+		newheronode = x.next
+		x.next = newheronode
+	}
+
+}
+
+func Delhernode(head *heronode, id int) {
+	heee := head
+	flag := false
+	for {
+		if heee.next == nil {
+			break
+		} else if heee.next.no == id {
+			flag = true
+			break
+		}
+		heee = heee.next
+	}
+	if flag {
+		heee.next = heee.next.next
+	} else {
+		fmt.Println("sorry dont id")
+	}
+}
+
+func listheronode(x *heronode) {
+	temp := x
+	if temp.next == nil {
+		fmt.Println("nothing")
+		return
+	}
+	for {
+		fmt.Printf("[%d ,%s %s]==>", temp.next.no, temp.next.name,
+			temp.next.nickname)
+		temp = temp.next
+		if temp.next == nil {
+			break
+		}
+	}
+
+}
+
+func main() {
+	herd := &heronode{}
+	herd1 := &heronode{
+		no:       1,
+		name:     "aaww",
+		nickname: "wwww",
+	}
+
+	inserhero(herd, herd1)
+	//inserhero2(herd,herd1)
+	//Delhernode(herd,2)
+	listheronode(herd)
+}
